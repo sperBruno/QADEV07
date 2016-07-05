31ded615b2d7035857692e3200b38d60ab7a7754
HenrrySalinasJala <HenrrySalinas@fundacion-jala.org>
2016-07-05 12:54:33 -0400

    adding api class files

---
+++ PivotalFinalTest/src/test/java/com/fundacionjala/PivotalFinalTest/cucumber/stepdefinitions/Asserts.java
@@ -0,0 +1,19 @@
+package com.fundacionjala.PivotalFinalTest.cucumber.stepdefinitions;
+
+import cucumber.api.java.en.And;
+
+import static org.junit.Assert.assertEquals;
+
+public class Asserts {
+
+    private ApiResources api;
+
+    public Asserts(ApiResources api) {
+        this.api = api;
+    }
+
+    @And("^The (?:project|story) name should be equals? to (.*)$")
+    public void theProjectShouldBeUpdated(String expectedName) {
+        assertEquals(expectedName, api.getResponse().path("name"));
+    }
+}
