ca83b960a7aa5ea9dc3528b85f5de8202320d0ef
Carlos Gonzales <carledriss@gmail.com>
2016-07-07 19:03:50 -0400

    Clean code

--- src/main/java/com/fundacionjala/pivotal/framework/selenium/RemoteBrowser.java
+++ src/main/java/com/fundacionjala/pivotal/framework/selenium/RemoteBrowser.java
@@ -3,40 +3,36 @@ package com.fundacionjala.pivotal.framework.selenium;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.fundacionjala.pivotal.framework.util.PropertiesInfo;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import static com.fundacionjala.pivotal.framework.util.PropertiesInfo.getInstance;
-
 public class RemoteBrowser implements IDriver {
 
     private static final Logger LOGGER = Logger.getLogger(RemoteBrowser.class.getSimpleName());
 
-    private static final String CAPABILITY_NAME = "name";
+    private static final PropertiesInfo PROPERTIES_INFO = PropertiesInfo.getInstance();
 
-    private static final String REMOTE_URL_AUTH = "@ondemand.saucelabs.com:80/wd/hub";
+    private static final String CAPABILITY_NAME = "name";
 
     private static final String HTTP_PROXY_HOST = "http.proxyHost";
 
     private static final String HTTP_PROXY_PORT = "http.proxyPort";
 
     public WebDriver initDriver() {
-        System.getProperties().put(HTTP_PROXY_HOST, getInstance().getProxyHost());
-        System.getProperties().put(HTTP_PROXY_PORT, getInstance().getProxyPort());
-        URL url = null;
+        System.getProperties().put(HTTP_PROXY_HOST, PROPERTIES_INFO.getProxyHost());
+        System.getProperties().put(HTTP_PROXY_PORT, PROPERTIES_INFO.getProxyPort());
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(CapabilityType.BROWSER_NAME, getInstance().getRemoteBrowser());
-        caps.setCapability(CapabilityType.VERSION, getInstance().getBrowserVersion());
-        caps.setCapability(CapabilityType.PLATFORM, getInstance().getPlatform());
-        caps.setCapability(CAPABILITY_NAME, getInstance().getRemoteTestName());
-        String sauceUrl = new StringBuilder().append("http://")
-                .append(getInstance().getSauceUser())
-                .append(":")
-                .append(getInstance().getSauceAccessKey())
-                .append(REMOTE_URL_AUTH).toString();
+        caps.setCapability(CapabilityType.BROWSER_NAME, PROPERTIES_INFO.getRemoteBrowser());
+        caps.setCapability(CapabilityType.VERSION, PROPERTIES_INFO.getBrowserVersion());
+        caps.setCapability(CapabilityType.PLATFORM, PROPERTIES_INFO.getPlatform());
+        caps.setCapability(CAPABILITY_NAME, PROPERTIES_INFO.getRemoteTestName());
+        final String sauceUrl = String.format("http://%s:%s@ondemand.saucelabs.com:80/wd/hub",
+                PROPERTIES_INFO.getSauceUser(), PROPERTIES_INFO.getSauceAccessKey());
+        URL url = null;
         try {
             url = new URL(sauceUrl);
         } catch (MalformedURLException e) {
