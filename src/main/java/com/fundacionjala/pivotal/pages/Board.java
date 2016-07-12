4915a65f903df6b052a09135659ed403c9dff946
Rosario Garcia <Rosario.Garcia@fundacion-jala.org>
2016-07-12 14:55:54 -0400

    add task

---
+++ src/main/java/com/fundacionjala/pivotal/pages/Board.java
@@ -0,0 +1,12 @@
+package com.fundacionjala.pivotal.pages;
+
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by RosarioGarcia on 7/12/2016.
+ */
+public class Board extends BasePage {
+    //@By
+    protected WebElement buttonAddStory;
+
+}
---
+++ src/main/java/com/fundacionjala/pivotal/pages/Icebox.java
@@ -0,0 +1,7 @@
+package com.fundacionjala.pivotal.pages;
+
+/**
+ * Created by RosarioGarcia on 7/12/2016.
+ */
+public class Icebox extends Board {
+}
