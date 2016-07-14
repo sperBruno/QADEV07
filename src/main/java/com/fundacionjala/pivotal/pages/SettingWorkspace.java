46677e12284a9487c2656b7622a565e06d44a196
DanielRGonzalesV <drgvandroid@gmail.com>
2016-07-14 18:17:02 -0400

    workspace add Sidebar and Delete

---
+++ src/main/java/com/fundacionjala/pivotal/pages/SettingWorkspace.java
@@ -0,0 +1,30 @@
+package com.fundacionjala.pivotal.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Created by danielgonzales on 7/14/2016.
+ */
+public class SettingWorkspace {
+
+   private ToolBar toolBar;
+
+    @FindBy(id = "workspace_name")
+    private WebElement workspaceNameText;
+
+    @FindBy(id = "delete_link")
+    private WebElement deleteWorkspaceLink;
+
+
+
+    public SettingWorkspace (){
+        toolBar = new ToolBar ();
+    }
+
+
+
+    public ToolBar getToolBar() {
+        return toolBar;
+    }
+}
