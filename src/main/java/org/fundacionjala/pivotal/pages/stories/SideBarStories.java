c57ce5e2d9eb2c7fff207f3b7ffee21fb1e5fa89
sperBruno <sperbru797@gmail.com>
2016-07-19 17:06:16 -0400

    storie

--- src/main/java/org/fundacionjala/pivotal/pages/stories/SideBarStories.java
+++ src/main/java/org/fundacionjala/pivotal/pages/stories/SideBarStories.java
@@ -1,17 +1,12 @@
 package org.fundacionjala.pivotal.pages.stories;
 
-import java.util.concurrent.TimeUnit;
-
 import org.fundacionjala.pivotal.pages.login.BasePage;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import static org.fundacionjala.pivotal.framework.util.Constants.IMPLICIT_FAIL_WAIT_TIME;
-import static org.fundacionjala.pivotal.framework.util.Constants.IMPLICIT_WAIT_TIME;
-
 /**
  * This class contains the web elements from side bar
+ *
  * @author RosarioGarcia
  */
 public class SideBarStories extends BasePage {
@@ -21,16 +16,11 @@ public class SideBarStories extends BasePage {
 
     /**
      * Method to do click on the botton "Add Story"
+     *
      * @return a Story object
      */
     public Story clickOnAddStoryButton() {
-        try {
-            driver.manage().timeouts().implicitlyWait(IMPLICIT_FAIL_WAIT_TIME, TimeUnit.SECONDS);
-            addStoryButton.click();
-        } catch (NoSuchElementException e) {
-        } finally {
-            driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
-        }
+        addStoryButton.click();
         return new Story();
     }
 }
