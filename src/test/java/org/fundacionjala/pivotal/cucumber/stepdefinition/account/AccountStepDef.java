8da83c2ce40f7a258bb3ff63ca256d79abbb9f29
sperBruno <sperbru797@gmail.com>
2016-07-19 14:23:36 -0400

    update Account

--- src/test/java/org/fundacionjala/pivotal/cucumber/stepdefinition/account/AccountStepDef.java
+++ src/test/java/org/fundacionjala/pivotal/cucumber/stepdefinition/account/AccountStepDef.java
@@ -1,61 +1,70 @@
 package org.fundacionjala.pivotal.cucumber.stepdefinition.account;
 
-import java.util.Map;
-
-import org.fundacionjala.pivotal.cucumber.stepdefinition.login.LoginStepDef;
-import org.fundacionjala.pivotal.cucumber.stepdefinition.projects.ProjectsStepDef;
-import org.fundacionjala.pivotal.pages.accounts.Accounts;
-import org.fundacionjala.pivotal.pages.project.ProjectSteps;
-import org.fundacionjala.pivotal.pages.setting.Setting;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import org.apache.log4j.Logger;
+import org.fundacionjala.pivotal.cucumber.stepdefinition.login.LoginStepDef;
+import org.fundacionjala.pivotal.cucumber.stepdefinition.projects.ProjectsStepDef;
+import org.fundacionjala.pivotal.pages.accounts.AccountSetting;
+import org.fundacionjala.pivotal.pages.accounts.Accounts;
+import org.fundacionjala.pivotal.pages.setting.Setting;
 
 /**
+ * This class is used to execute the steps of Account scenarios.
  * Created by brunobarrios on 7/18/2016.
  */
 public class AccountStepDef {
 
-    private static Logger LOGGER = Logger.getLogger(AccountStepDef.class.getName());
-    private static Setting SETTING;
+    private Accounts account;
 
     private LoginStepDef loginStepDef;
 
     private ProjectsStepDef projectsStepDef;
 
     private String accountName;
-    Accounts account;
-    private Map<ProjectSteps, Object> valuesMap;
 
+    /**
+     * This class receives LoginStepDef and ProjectStepDef as a parameters.
+     * @param loginStepDef
+     * @param projectsStepDef
+     */
     public AccountStepDef(LoginStepDef loginStepDef, ProjectsStepDef projectsStepDef) {
         this.loginStepDef = loginStepDef;
         this.projectsStepDef = projectsStepDef;
     }
-//
-//    @And("^I delete the of the project$")
-//    public void iDeleteTheAccountOfTheProject() {
-//        SETTING = StoriesStepsDef.getProject().clickSettingTab();
-//         account = SETTING.getSideBar().clickGeneralSetting().clickAccountLink();
-//        AccountSetting accountSetting = account.getToolBarAccount().clickSettingTab();
-//        accountName = account.getDeleteAccountMessage();
-//        accountSetting.deleteAccount();
-//    }
+
+    /**
+     * This method is used to get into the account page from dashboard.
+     */
+    @Given("^I get into account Page$")
+    public void iGetIntoAccountPage()  {
+        account = loginStepDef.getDashboard().selectAccountOption();
+    }
+
+    /**
+     * This method will delete an account inside of a project.
+     */
+    @And("^I delete the account of the project$")
+    public void iDeleteTheAccountOfTheProject() {
+        Setting setting = projectsStepDef.getProject().clickSettingTab();
+        account = setting.getSideBar().clickGeneralSetting().clickAccountLink();
+        AccountSetting accountSetting = account.getToolBarAccount().clickSettingTab();
+        accountName = accountSetting.getAccountName();
+        accountSetting.deleteAccount();
+    }
+
+    public Accounts getAccount() {
+        return account;
+    }
+
+    public ProjectsStepDef getProjectsStepDef() {
+        return projectsStepDef;
+    }
 
     public String getAccountName() {
-        return accountName;
+        return accountName.toString();
     }
 
     public Accounts getAccountPage() {
         return new Accounts();
-    }
-
-    @Given("^I get into account Page$")
-    public void iGetIntoAccountPage() throws Throwable {
-        account = loginStepDef.getDashboard().selectAccountOption();
-    }
-
-    @And("^I delete the account of the project$")
-    public void iDeleteTheAccountOfTheProject() {
-        Accounts a = account.manageAccount().clickSettingTab().deleteAccount();
     }
 }
--- src/test/java/org/fundacionjala/pivotal/cucumber/stepdefinition/projects/ProjectAssert.java
+++ src/test/java/org/fundacionjala/pivotal/cucumber/stepdefinition/projects/ProjectAssert.java
@@ -1,14 +1,16 @@
 package org.fundacionjala.pivotal.cucumber.stepdefinition.projects;
 
-import org.fundacionjala.pivotal.api.Mapper;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 
+import org.fundacionjala.pivotal.api.Mapper;
+
 import static org.junit.Assert.assertEquals;
 
 /**
+ * This class is used to validate all the scenarios related to a Project.
  * Created by BrunoBarrios on 7/7/2016.
  */
 public class ProjectAssert {
@@ -19,11 +21,23 @@ public class ProjectAssert {
 
     private ProjectSettingsStepDef projectSettingsStepDef;
 
+    /**
+     * This class receives ProjectStepDef and ProjectSettingsStepDef as parameters.
+     *
+     * @param projectsStepDef
+     * @param projectSettingsStepDef
+     */
     public ProjectAssert(ProjectsStepDef projectsStepDef, ProjectSettingsStepDef projectSettingsStepDef) {
         this.projectsStepDef = projectsStepDef;
         this.projectSettingsStepDef = projectSettingsStepDef;
     }
 
+    /**
+     * This class is used to validate whether the project description is equals as an expected
+     * project description.
+     *
+     * @param expectedValue is the expected description of the project
+     */
     @And("^The description projects should be equals (.*)$")
     public void theDescriptionProjectsShouldBeEqualsA(String expectedValue) {
         assertEquals(expectedValue, projectSettingsStepDef.getGeneralSettingForm().getDescriptionText());
@@ -40,6 +54,11 @@ public class ProjectAssert {
         assertEquals(expectMessage, projectSettingsStepDef.getDashboard().getMessageTextDelete());
     }
 
+    /**
+     * This class is used to validate whether the project name is equals as expected project name.
+     *
+     * @param expectedValue is the expected project name or title.
+     */
     @Then("^The project title should be equals (.*)$")
     public void theProjectTitleShouldBeEqualsProjectSeleniumTest(String expectedValue) {
         LOGGER.info("title project " + projectsStepDef.getProject().getTitle());
