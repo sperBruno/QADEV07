c7a11860cd3d15ed7bded24dd2d8d5017cbf2d47
HenrrySalinasJala <HenrrySalinas@fundacion-jala.org>
2016-08-04 21:21:58 -0400

    modification on remote test classes

--- src/main/java/org/fundacionjala/pivotal/framework/selenium/RemoteBrowser.java
+++ src/main/java/org/fundacionjala/pivotal/framework/selenium/RemoteBrowser.java
@@ -6,11 +6,9 @@ import java.net.URL;
 import org.apache.log4j.Logger;
 import org.fundacionjala.pivotal.framework.util.PropertiesInfo;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
-import static org.fundacionjala.pivotal.framework.util.CommonMethods.setCapabilities;
-import static org.fundacionjala.pivotal.framework.util.Constants.HTTP_PROXY_HOST;
-import static org.fundacionjala.pivotal.framework.util.Constants.HTTP_PROXY_PORT;
 
 /**
  * This class initialize the Remote Selenium Web Driver given the required values in properties file
@@ -23,6 +21,12 @@ public class RemoteBrowser implements IDriver {
 
     private static final PropertiesInfo PROPERTIES_INFO = PropertiesInfo.getInstance();
 
+    private static final String CAPABILITY_NAME = "name";
+
+    private static final String HTTP_PROXY_HOST = "http.proxyHost";
+
+    private static final String HTTP_PROXY_PORT = "http.proxyPort";
+
     /**
      * {@inheritDoc}
      */
@@ -30,14 +34,19 @@ public class RemoteBrowser implements IDriver {
     public WebDriver initDriver() {
         System.getProperties().put(HTTP_PROXY_HOST, PROPERTIES_INFO.getProxyHost());
         System.getProperties().put(HTTP_PROXY_PORT, PROPERTIES_INFO.getProxyPort());
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(CapabilityType.BROWSER_NAME, PROPERTIES_INFO.getRemoteBrowser());
+        caps.setCapability(CapabilityType.VERSION, PROPERTIES_INFO.getBrowserVersion());
+        caps.setCapability(CapabilityType.PLATFORM, PROPERTIES_INFO.getPlatform());
+        caps.setCapability(CAPABILITY_NAME, PROPERTIES_INFO.getRemoteTestName());
+        final String sauceUrl = String.format(PROPERTIES_INFO.getRemoteTestHostUrl(),
+                PROPERTIES_INFO.getRemoteUser(), PROPERTIES_INFO.getRemoteAccessKey());
         URL url = null;
         try {
-            final String sauceUrl = String.format(PROPERTIES_INFO.getRemoteTestHostUrl(),
-                    PROPERTIES_INFO.getRemoteUser(), PROPERTIES_INFO.getRemoteAccessKey());
             url = new URL(sauceUrl);
         } catch (MalformedURLException e) {
             LOGGER.warn("The url is not correct" + e);
         }
-        return new RemoteWebDriver(url, setCapabilities());
+        return new RemoteWebDriver(url, caps);
     }
 }
